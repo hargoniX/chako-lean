9e2f0b37e107a7529a353db1952bbfe4de1e19e8
Henrik Böving <hargonix@gmail.com>
2025-12-07 13:40:29 +0100

    eval ready

--- Test/Eval/Perf.lean
+++ Test/Eval/Perf.lean
@@ -1,27 +1,28 @@
 import Chako
-import Test.Eval.RbSet
-set_option maxHeartbeats 200000000
+
 set_option Elab.async false
+set_option maxHeartbeats 200000000
 #exit
+#eval_chako_perf_module Init.Data.Array.Basic "results/perf/array_basic.csv"
+#eval_chako_perf_module Init.Data.Array.Lemmas "results/perf/array_lemmas.csv"
+
+#eval_chako_perf_module Init.Data.BitVec.Basic "results/perf/bitvec_basic.csv"
+#eval_chako_perf_module Init.Data.BitVec.Lemmas "results/perf/bitvec_lemmas.csv"
+
+#eval_chako_perf_module Init.Data.Fin.Basic "results/perf/fin_basic.csv"
+#eval_chako_perf_module Init.Data.Fin.Lemmas "results/perf/fin_lemmas.csv"
+
+#eval_chako_perf_module Init.Data.Int.Basic "results/perf/int_basic.csv"
+#eval_chako_perf_module Init.Data.Int.Lemmas "results/perf/int_lemmas.csv"
+
 #eval_chako_perf_module Init.Data.List.Basic "results/perf/list_basic.csv"
 #eval_chako_perf_module Init.Data.List.Lemmas "results/perf/list_lemmas.csv"
 
 #eval_chako_perf_module Init.Data.Nat.Basic "results/perf/nat_basic.csv"
 #eval_chako_perf_module Init.Data.Nat.Lemmas "results/perf/nat_lemmas.csv"
-
-#eval_chako_perf_module Init.Data.Int.Basic "results/perf/int_basic.csv"
-#eval_chako_perf_module Init.Data.Int.Lemmas "results/perf/int_lemmas.csv"
-
-#eval_chako_perf_module Init.Data.Fin.Basic "results/perf/fin_basic.csv"
-#eval_chako_perf_module Init.Data.Fin.Lemmas "results/perf/fin_lemmas.csv"
+#eval_chako_perf_module Init.Data.Nat.Gcd "results/perf/nat_gcd.csv"
 
 #eval_chako_perf_module Init.Data.Option.Basic "results/perf/option_basic.csv"
 #eval_chako_perf_module Init.Data.Option.Lemmas "results/perf/option_lemmas.csv"
 
-#eval_chako_perf_module Init.Data.BitVec.Basic "results/perf/bitvec_basic.csv"
-#eval_chako_perf_module Init.Data.BitVec.Lemmas "results/perf/bitvec_lemmas.csv"
-
-#eval_chako_perf_module Init.Data.Array.Basic "results/perf/array_basic.csv"
-#eval_chako_perf_module Init.Data.Array.Lemmas "results/perf/array_lemmas.csv"
-
-#eval_chako_perf_module Test.Eval.RbSet "results/perf/rbset.csv"
+#eval_chako_perf_module Std.Data.TreeMap.Lemmas "results/perf/treemap_lemmas.csv"
--- Test/Eval/Sound.lean
+++ Test/Eval/Sound.lean
@@ -1,28 +1,28 @@
 import Chako
-import Test.Eval.RbSet
 
 set_option Elab.async false
 set_option maxHeartbeats 200000000
 #exit
+#eval_chako_sound_module Init.Data.Array.Basic "results/sound/array_basic.csv"
+#eval_chako_sound_module Init.Data.Array.Lemmas "results/sound/array_lemmas.csv"
+
+#eval_chako_sound_module Init.Data.BitVec.Basic "results/sound/bitvec_basic.csv"
+#eval_chako_sound_module Init.Data.BitVec.Lemmas "results/sound/bitvec_lemmas.csv"
+
+#eval_chako_sound_module Init.Data.Fin.Basic "results/sound/fin_basic.csv"
+#eval_chako_sound_module Init.Data.Fin.Lemmas "results/sound/fin_lemmas.csv"
+
+#eval_chako_sound_module Init.Data.Int.Basic "results/sound/int_basic.csv"
+#eval_chako_sound_module Init.Data.Int.Lemmas "results/sound/int_lemmas.csv"
+
 #eval_chako_sound_module Init.Data.List.Basic "results/sound/list_basic.csv"
 #eval_chako_sound_module Init.Data.List.Lemmas "results/sound/list_lemmas.csv"
 
 #eval_chako_sound_module Init.Data.Nat.Basic "results/sound/nat_basic.csv"
 #eval_chako_sound_module Init.Data.Nat.Lemmas "results/sound/nat_lemmas.csv"
-
-#eval_chako_sound_module Init.Data.Int.Basic "results/sound/int_basic.csv"
-#eval_chako_sound_module Init.Data.Int.Lemmas "results/sound/int_lemmas.csv"
-
-#eval_chako_sound_module Init.Data.Fin.Basic "results/sound/fin_basic.csv"
-#eval_chako_sound_module Init.Data.Fin.Lemmas "results/sound/fin_lemmas.csv"
+#eval_chako_sound_module Init.Data.Nat.Gcd "results/sound/nat_gcd.csv"
 
 #eval_chako_sound_module Init.Data.Option.Basic "results/sound/option_basic.csv"
 #eval_chako_sound_module Init.Data.Option.Lemmas "results/sound/option_lemmas.csv"
 
-#eval_chako_sound_module Init.Data.BitVec.Basic "results/sound/bitvec_basic.csv"
-#eval_chako_sound_module Init.Data.BitVec.Lemmas "results/sound/bitvec_lemmas.csv"
-
-#eval_chako_sound_module Init.Data.Array.Basic "results/sound/array_basic.csv"
-#eval_chako_sound_module Init.Data.Array.Lemmas "results/sound/array_lemmas.csv"
-
-#eval_chako_sound_module Test.Eval.RbSet "results/sound/rbset.csv"
+#eval_chako_sound_module Std.Data.TreeMap.Lemmas "results/sound/treemap_lemmas.csv"
